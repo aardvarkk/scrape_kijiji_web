3f0b1eaf3dee81346cb2130587aa0e6c25174df6
Ian Clarkson <clarksonian@gmail.com>
2013-02-06 12:29:43 -0500

    Initial commit. Blank Rails project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ScrapeKijijiWeb::Application.config.secret_token = 'aed7c774f568b698896d28e90c51075329978c10d4e2f62b2cd12e7c5f6f9265016ff9e8df0e421b92fea254191a5ee534439836b352efde9d7ce25234456530'
