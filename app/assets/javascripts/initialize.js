877888a508e671a060c22d822608eb93f82fd719
Ian Clarkson <clarksonian@gmail.com>
2013-02-07 16:54:45 -0500

    Showing markers for all properties.

--- app/assets/javascripts/initialize.js
+++ app/assets/javascripts/initialize.js
@@ -14,15 +14,18 @@ window.onload = function() {
 	data_url = document.getElementById('map_canvas').getAttribute('data-url');
 
 	// draw the overlays once we retrieve the data
-	$.getJSON(data_url, function(listings) { drawOverlays(map, myLatlng, listings); });
+	$.getJSON(data_url, function(listings) 
+		{ 
+			drawOverlays(map, myLatlng, listings); 
 
-	// draw a marker for each listing
-	//var markers = new Array();
-	//for (var i = 0; i < listings.length; ++i) {
-	//	markers[i] = new google.maps.Marker({
-	//	position : listings[i].pos,
-	//	map : map,
-	//	title : listings[i].name
-	//	});
-	//}
+			// draw a marker for each listing
+			var markers = new Array();
+			for (var i = 0; i < listings.length; ++i) {
+				markers[i] = new google.maps.Marker({
+				position : new google.maps.LatLng(listings[i]['lat'], listings[i]['lon']),
+				map : map,
+				title : listings[i]['title']
+				});
+			}
+		});
 }
