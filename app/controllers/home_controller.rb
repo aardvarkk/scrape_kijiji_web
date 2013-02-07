ab1971ae3415f470e6c857016db2b1e1c43992cc
Ian Clarkson <clarksonian@gmail.com>
2013-02-07 14:27:29 -0500

    Splitting into maps and listing (index)

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,10 @@
 class HomeController < ApplicationController
 
   def index
-  	@rentals = Rental.order("pubdate DESC").all
+  	@rentals = Rental.order("pubdate DESC").limit(10).all
+  end
+
+  def map
   end
 
 end
